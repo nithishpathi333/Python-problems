5818f2d5ca7311f667bf84612a24e86df6bfd522
nithishpathi333 <162962458+nithishpathi333@users.noreply.github.com>
2024-08-02 12:48:35 +0530

    Polymorphism.py

---
+++ Polymorphism.py
@@ -0,0 +1,13 @@
+class father:
+    def role(self):
+        print("taking care of family")
+class officeemployee:
+    def role(self):
+        print("working hard in office")
+      
+father().role()
+officeemployee().role()
+
+output: 
+        taking care of family
+        working hard in office
