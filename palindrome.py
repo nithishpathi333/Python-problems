a6e6fc5fd3494c8e8da3517fc9f16c2e75d5d0a8
nithishpathi333 <162962458+nithishpathi333@users.noreply.github.com>
2024-08-03 14:23:55 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+a=input("Enter a input:")
+if a==a[::-1]:
+    print("a is a palindrone")
+else:
+    print("a is not a palindrone")
+
+Output: 
+
+        Enter a input:racecar
+        a is a palindrone
