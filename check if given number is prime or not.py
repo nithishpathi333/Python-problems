d6c62a5134352d3309f8639b4c9b25acfbb994d6
nithishpathi333 <162962458+nithishpathi333@users.noreply.github.com>
2024-08-03 23:07:37 +0530

    check if given number is prime or not.py

---
+++ check if given number is prime or not.py
@@ -0,0 +1,13 @@
+Q: program to find if given number is prime or not?
+    a=int(input())
+    
+    for i in range(2,a):
+        if a%i==0:
+            print(f"{a} is a not prime number")
+            break
+        else:
+            print(f"{a} is a prime number")
+
+output:  
+         10
+         10 is a not prime number
