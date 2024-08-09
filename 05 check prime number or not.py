46e08d1b2429f0ac182965c626fdb5e5de0ef7ac
nithishpathi333 <162962458+nithishpathi333@users.noreply.github.com>
2024-08-09 13:06:50 +0530

    check prime number or not.py

--- 05 check prime number or not.py
+++ 05 check prime number or not.py
@@ -4,7 +4,7 @@ Q: check whether given number is prime number or not?
     a=18762
     for i in range(2,a):
        if a%i==0:
-           count = 1
+           count += 1
     if count==0:
         print("prime")
     else :
