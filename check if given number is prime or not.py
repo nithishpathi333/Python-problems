9257c5e76463f22b45bc565fdc7a99ed824b7fec
nithishpathi333 <162962458+nithishpathi333@users.noreply.github.com>
2024-08-09 12:58:04 +0530

    check if given number is prime or not.py

--- check if given number is prime or not.py
+++ check if given number is prime or not.py
@@ -5,8 +5,8 @@ Q: program to find if given number is prime or not?
         if a%i==0:
             print(f"{a} is a not prime number")
             break
-        else:
-            print(f"{a} is a prime number")
+    else:
+        print(f"{a} is a prime number")
 
 output:  
          10
